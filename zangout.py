ab0c0c57d2340d1473108008faf0c619362463e8
ajinkya mhatre <ajinkyamhatre7642@gmail.com>
2023-05-07 10:56:17 +0530

    using proper naming

--- zangout.py
+++ zangout.py
@@ -1,52 +1,55 @@
 import MySQLdb
 from flask import *
+
+
 def db():
-	return MySQLdb.connect('localhost','root','qwerty@123','test')
-app=Flask(__name__)
+    return MySQLdb.connect('localhost', 'root', 'qwerty@123', 'test')
+
+
+def get_user_list(id=None):
+    connection = db()
+    cursor = connection.cursor()
+    query = 'select * from mytable;'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    output = []
+    for user in data:
+        user_data = {'id': user[0], 'name': user[1], 'address': user[2], 'phone': user[3]}
+        if id and id == user_data['id']:
+            output.append(user_data)
+        else:
+            output.append(user_data)
+    connection.close()
+    return output
+
+
+app = Flask(__name__)
+
 
 @app.route('/UserService/Users')
-def userall():
-	connection = db()
-	cursor = connection.cursor()
-	query = 'select * from mytable;'
-	cursor.execute(query)
-	data = cursor.fetchall()
-	output = []
-	for user in data:
-		user_data = {'id': user[0], 'name': user[1], 'address': user[2], 'phone': user[3]}
-		output.append(user_data)
-	connection.close()
-	return jsonify(output)
+def get_all_users():
+    output = get_user_list()
+    return jsonify(output)
+
+
 @app.route('/UserService/Users/<int:id>')
-def userone(id):
-	con = db()
-	cr = con.cursor()
-	query = 'select * from mytable;'
-	cr.execute(query)
-	data = cr.fetchall()
-	ans =[]
-	for i in data:
-		j = {'id' : i[0], 'name' : i[1], 'address' : i[2], 'phone' : i[3] }
-		ans.append(j)
-	if (id > len(ans)) or (id < 1):
-		op = {'error' : 'array index out of order'}
-	else :
-		op = ans[id-1]
-	con.close()
-	return jsonify(op)
-
-@app.route('/UserService/Users', methods =['GET','PUT'])
-def userput():
-	con = db()
-	cr = con.cursor()
-	if request.method == 'PUT':
-		data = request.json
-		query = "insert into mytable (name,address,phone) values ('%s','%s','%s')"%(data['name'],data['address'],data['phone'])
-		cr.execute(query)
-		con.commit()
-	con.close()
-	return "ok"
-if __name__=="__main__":
-        app.run('localhost',8080,debug=True)
+def get_user(id):
+    output = get_user_list(id)
+    return jsonify(output)
 
 
+@app.route('/UserService/Users', methods=['GET', 'PUT'])
+def update_user():
+    con = db()
+    cr = con.cursor()
+    if request.method == 'PUT':
+        data = request.json
+        query = f"insert into mytable (name,address,phone) values ('{data['name']}','{data['address']}','{data['phone']}')"
+        cr.execute(query)
+        con.commit()
+    con.close()
+    return jsonify({"response": "ok"})
+
+
+if __name__ == "__main__":
+    app.run('localhost', 8080, debug=True)
